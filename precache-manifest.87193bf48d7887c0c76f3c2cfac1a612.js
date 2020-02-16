df45d42f31269519929ff2d00f523a7b57c249a4
Alay Chaniyara <alaychaniyara@gmail.com>
2020-02-16 16:16:00 -0500

    Updates

---
+++ precache-manifest.87193bf48d7887c0c76f3c2cfac1a612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a53ce3b9906934904648881cfab923",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "c386b78d4085b230c429",
+    "url": "/myportfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ab61b57436b31cf554d6",
+    "url": "/myportfolio/static/js/2.7513cc15.chunk.js"
+  },
+  {
+    "revision": "af1b71dc8f7cc14f3b4c1f4697374759",
+    "url": "/myportfolio/static/js/2.7513cc15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c386b78d4085b230c429",
+    "url": "/myportfolio/static/js/main.ba236218.chunk.js"
+  },
+  {
+    "revision": "a4d15c5a767abea188ff",
+    "url": "/myportfolio/static/js/runtime-main.50694e15.js"
+  }
+]);
